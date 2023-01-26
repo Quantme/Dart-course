b164bae1bb5f17b1f5f2bfc2cd0ee75a6e1e21a5
Quantme <ivan.tc@gmail.com>
2023-01-26 17:44:35 -0600

    Comments added.

--- comments/bin/comments.dart
+++ comments/bin/comments.dart
@@ -1,5 +1,12 @@
+/*
+This imports a file
+and uses it as a variable.
+*/
 import 'package:comments/comments.dart' as comments;
 
+// This is the starting point in the application.
 void main(List<String> arguments) {
+
+  // This prints out to the screen.
   print('Hello world: ${comments.calculate()}!');
 }
