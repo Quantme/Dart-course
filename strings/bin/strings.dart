1e733c548551221efea7d77a82d32a958af588b3
Quantme <ivan.tc@gmail.com>
2023-01-26 20:36:38 -0600

    Strings.

---
+++ strings/bin/strings.dart
@@ -0,0 +1,30 @@
+import 'package:strings/strings.dart' as strings;
+
+void main(List<String> arguments) {
+  String hello = 'hello world';
+  print(hello);
+
+  String name = 'John Doe';
+  print('Hello $name');
+
+  // Get a substring.
+  String firstname = name.substring(0, 5);
+  print(firstname);
+
+  // Get the index of a string.
+  int index = name.indexOf(' ');
+  String lastname = name.substring(index).trim();
+  print('lastname = $lastname');
+
+  // Length
+  print(name.length);
+
+  // Contains
+  print(name.contains('oe'));
+
+  // Create a list
+  List parts = name.split(' ');
+  print(parts);
+  print(parts[0]);
+  print(parts[1]);
+}
