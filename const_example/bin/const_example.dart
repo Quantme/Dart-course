4a49bd9b5f8ae322a8d84194e5065a4d97b67e7f
Quantme <ivan.tc@gmail.com>
2023-01-27 19:17:22 -0600

    Const example.

---
+++ const_example/bin/const_example.dart
@@ -0,0 +1,6 @@
+void main(List<String> arguments) {
+  String hello = 'hello';
+  const world = 'World';
+  // world = 'Doe'; // Constant variables can't be assigned a value.
+  print(hello + ' ' + world);
+}
