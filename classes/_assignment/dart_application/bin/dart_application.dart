d29e0a8e6ae445f0abb4b91cfd887e10b1e4e153
Quantme <ivan.tc@gmail.com>
2023-03-11 22:31:15 -0600

    Assignment edited

--- classes/_assignment/dart_application/bin/dart_application.dart
+++ classes/_assignment/dart_application/bin/dart_application.dart
@@ -1,6 +1,3 @@
-import 'package:dart_application/employee.dart';
-
-void main(List<String> arguments) {
 /*
 Assignment:
 - Create an employee class.
@@ -8,6 +5,9 @@ Assignment:
   set those strings in the constructor.
 - Then make a function that prints out the employee's name and position.
 */
+import 'package:dart_application/employee.dart';
+
+void main(List<String> arguments) {
 
   Employee kate = Employee('Kate', 'Sales');
 
